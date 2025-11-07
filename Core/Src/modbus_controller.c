cac9238306ebec2bfb933b5f46ff0f9063736245
KreedM <blackknight3377@gmail.com>
2025-11-06 20:50:09 -0800

    Oops

--- Core/Src/modbus_controller.c
+++ Core/Src/modbus_controller.c
@@ -1,13 +1,23 @@
 #include "modbus_controller.h"
 #include "modbus_io.h"
 
+void modbus_controller_init(void) {
+	modbus_controller_address = 0xFF;
+}
+
 void modbus_controller_tick(void) {
 	modbus_controller_buffer_size = modbus_io_read(modbus_controller_buffer);
 
 	if(modbus_controller_buffer_size < MODBUS_MIN_MESSAGE_BYTES)
 		return;
 
-	if(modbus_controller_buffer[modbus_controller_buffer_size - 1] != CRC_calc(modbus_controller_buffer_size))
+	if(modbus_controller_buffer[0] != modbus_controller_address)
+		return;
+
+	uint16_t CRC = modbus_controller_buffer[modbus_controller_buffer_size - 2] << 8 |
+			   modbus_controller_buffer[modbus_controller_buffer_size - 1] ;
+
+	if(CRC != CRC_calc(modbus_controller_buffer_size))
 		return;
 
 	process_modbus_message();
