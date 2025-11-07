cfcec5cecf6a8ae5c2b2c72d6aee55b748f1d8d6
KreedM <blackknight3377@gmail.com>
2025-11-06 20:35:20 -0800

    Added modbus constants

--- Core/Inc/modbus_controller.h
+++ Core/Inc/modbus_controller.h
@@ -2,11 +2,14 @@
 #define MODBUS_CONTROLLER_H
 
 #include "modbus_io.h"
+#include "modbus_constants.h"
 
 static volatile uint8_t modbus_controller_buffer[MODBUS_IO_BUFFER_SIZE];
 
-void process_modbus_message(void);
 void modbus_controller_tick(void);
+
+void process_modbus_message(void);
+
 uint16_t CRC_calc(uint8_t *data);
 
 #endif
